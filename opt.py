f118226fb9ca598d18b3a497a03f477b96fbd050
quan5609 <bvmquan@gmail.com>
2022-12-21 11:16:49 +0900

    clean opt

--- opt.py
+++ opt.py
@@ -23,18 +23,29 @@ def get_opts():
         help="whether images are taken in spheric poses (for llff)",
     )
 
-    parser.add_argument("--N_emb_xyz", type=int, default=10, help="number of frequencies in xyz positional encoding")
-    parser.add_argument("--N_emb_dir", type=int, default=4, help="number of frequencies in dir positional encoding")
-    parser.add_argument("--N_samples", type=int, default=128, help="number of coarse samples")
-    parser.add_argument("--N_importance", type=int, default=0, help="number of additional fine samples")
-    parser.add_argument("--use_disp", default=False, action="store_true", help="use disparity depth sampling")
-    parser.add_argument("--perturb", type=float, default=1.0, help="factor to perturb depth sampling points")
-    parser.add_argument("--noise_std", type=float, default=1.0, help="std dev of noise added to regularize sigma")
+    parser.add_argument("--N_emb_xyz", type=int, default=10,
+                        help="number of frequencies in xyz positional encoding")
+    parser.add_argument("--N_emb_dir", type=int, default=4,
+                        help="number of frequencies in dir positional encoding")
+    parser.add_argument("--N_samples", type=int, default=128,
+                        help="number of coarse samples")
+    parser.add_argument("--N_importance", type=int, default=0,
+                        help="number of additional fine samples")
+    parser.add_argument("--use_disp", default=False,
+                        action="store_true", help="use disparity depth sampling")
+    parser.add_argument("--perturb", type=float, default=1.0,
+                        help="factor to perturb depth sampling points")
+    parser.add_argument("--noise_std", type=float, default=1.0,
+                        help="std dev of noise added to regularize sigma")
 
-    parser.add_argument("--batch_size", type=int, default=1024, help="batch size")
-    parser.add_argument("--chunk", type=int, default=32 * 1024, help="chunk size to split the input to avoid OOM")
-    parser.add_argument("--num_epochs", type=int, default=16, help="number of training epochs")
-    parser.add_argument("--num_gpus", type=int, default=1, help="number of gpus")
+    parser.add_argument("--batch_size", type=int,
+                        default=1024, help="batch size")
+    parser.add_argument("--chunk", type=int, default=32 *
+                        1024, help="chunk size to split the input to avoid OOM")
+    parser.add_argument("--num_epochs", type=int, default=16,
+                        help="number of training epochs")
+    parser.add_argument("--num_gpus", type=int,
+                        default=1, help="number of gpus")
 
     parser.add_argument(
         "--ckpt_path", type=str, default=None, help="pretrained checkpoint to load (including optimizers, etc)"
@@ -54,8 +65,10 @@ def get_opts():
         "--optimizer", type=str, default="adam", help="optimizer type", choices=["sgd", "adam", "radam", "ranger"]
     )
     parser.add_argument("--lr", type=float, default=5e-4, help="learning rate")
-    parser.add_argument("--momentum", type=float, default=0.9, help="learning rate momentum")
-    parser.add_argument("--weight_decay", type=float, default=0, help="weight decay")
+    parser.add_argument("--momentum", type=float,
+                        default=0.9, help="learning rate momentum")
+    parser.add_argument("--weight_decay", type=float,
+                        default=0, help="weight decay")
     parser.add_argument(
         "--lr_scheduler", type=str, default="steplr", help="scheduler type", choices=["steplr", "cosine", "poly"]
     )
@@ -67,14 +80,18 @@ def get_opts():
         "--warmup_epochs", type=int, default=0, help="Gradually warm-up(increasing) learning rate in optimizer"
     )
     ###########################
-    #### params for steplr ####
-    parser.add_argument("--decay_step", nargs="+", type=int, default=[20], help="scheduler decay step")
-    parser.add_argument("--decay_gamma", type=float, default=0.1, help="learning rate decay amount")
+    # params for steplr
+    parser.add_argument("--decay_step", nargs="+", type=int,
+                        default=[20], help="scheduler decay step")
+    parser.add_argument("--decay_gamma", type=float,
+                        default=0.1, help="learning rate decay amount")
     ###########################
-    #### params for poly ####
-    parser.add_argument("--poly_exp", type=float, default=0.9, help="exponent for polynomial learning rate decay")
+    # params for poly
+    parser.add_argument("--poly_exp", type=float, default=0.9,
+                        help="exponent for polynomial learning rate decay")
     ###########################
 
-    parser.add_argument("--exp_name", type=str, default="exp", help="experiment name")
+    parser.add_argument("--exp_name", type=str,
+                        default="exp", help="experiment name")
 
     return parser.parse_args()
